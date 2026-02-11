dddcfd6e3d6944fb6fc4fd2ecacdc177e697d637
Alex-Hasi2 <alexanderschumer@hotmail.com>
2026-02-11 08:48:54 +0100

    Added new combined_main Added Kurztest_1

--- Code/combineFiles.py
+++ Code/combineFiles.py
@@ -4,9 +4,15 @@
 
 import os
 
+
 def combine_latex_files(main_folder, main_file, output_file):
+
+    # Make sure that main.tex cannot be the output file
+    if main_file == output_file:
+        raise ValueError("The output file cannot be the same as the main file.")
+    
     main_file_path = os.path.join(main_folder, main_file)
-    with open(main_file_path, 'r') as mf:
+    with open(main_file_path, 'r', encoding='utf-8') as mf:
         main_content = mf.readlines()
 
     combined_content = []
@@ -16,9 +22,13 @@ def combine_latex_files(main_folder, main_file, output_file):
             section_name = line.strip()[9:-1]  # Extract the section name
             section_file = os.path.join(main_folder, f"{section_name}.tex")
             if os.path.exists(section_file):
+                print(f"Inserting content from {section_file}")
                 with open(section_file, 'r') as sf:
                     section_content = sf.read()
+                    # Insert comment % Start of <filename.tex> before the content and a comment % End of <filename.tex> after the content
+                    combined_content.append(f"% ---- Start of {section_name}.tex\n") 
                     combined_content.append(section_content + '\n')
+                    combined_content.append(f"% ---- End of {section_name}.tex\n")
             else:
                 print(f"Warning: Section file {section_file} not found.")
 
@@ -29,7 +39,7 @@ def combine_latex_files(main_folder, main_file, output_file):
 if __name__ == "__main__":
     main_file = 'main.tex'
     main_folder = r'C:/Users/alexsc31/Documents_privat/FH Campus/Clinical_Engineering/GithubRepo/Skriptum_Grundlagen_Physik_FH-Campus/'
-    output_file = 'combined_main.tex'
+    output_file = 'combined_main_2026-02.tex'
 
     
     combine_latex_files(main_folder, main_file, output_file)
